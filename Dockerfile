5c2ee6703adb93b22313f0d37406b7c67659168e
charles161 <fortloop.charles@gmail.com>
2020-07-12 00:56:06 +0530

    working on it

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,23 @@
-FROM node:12-slim
+FROM node:12.18.2
+# ENV DOCKERIZE_VERSION v0.6.1
+# RUN apk add --no-cache openssl \
+#     && wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+#     && tar -C /usr/local/bin -xzvf dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+#     && rm dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+# RUN apk add --no-cache bash bash-doc bash-completion
 RUN npm install pm2 -g
-RUN mkdir -p /usr/src/app
-RUN mkdir -p /usr/src/credentials
-WORKDIR /usr/src/app
-ENV DOCKERIZE_VERSION v0.6.1
-COPY package.json /usr/src/app/
-RUN npm install
-COPY aedes.js /usr/src/app/
-COPY ecosystem.config.js /usr/src/app/
-EXPOSE 7070 8120 8110
+RUN mkdir -p /usr/src/poco
+WORKDIR /usr/src/poco
+COPY package.json /usr/src/poco/
+# RUN apk add --no-cache --virtual .gyp \
+#         python \
+#         make \
+#         g++ \
+#     && npm install \
+#     && apk del .gyp
+COPY index.js /usr/src/poco/
+COPY pm2.config.js /usr/src/poco/
 ENV PM2_PUBLIC_KEY 7of45w7xe0y8bai
 ENV PM2_SECRET_KEY 9dou692e1qtcds8
-CMD ["pm2-runtime", "pm2.config.js"]
-VOLUME [ "/usr/src/credentials" ]
+# CMD ["pm2-runtime", "pm2.config.js"]
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var WebSocketServer = require("websocket").server;
+// var WebSocketServer = require("websocket").server;
 var http = require("http");
 const pid = process.pid;
 const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
   clientId: "my-app",
-  brokers: ["localhost:9092"],
+  brokers: ["2.tcp.ngrok.io:10345"],
 });
 
 const consumer = kafka.consumer({ groupId: "mqtt-cool" });
@@ -31,7 +31,7 @@ async function initKafka() {
       topic: "mqtt",
       messages: [{ value: "Hello KafkaJS user!" }],
     });
-  }, 3000);
+  }, 10000);
 
   // await producer.disconnect();
 }
@@ -43,38 +43,6 @@ var server = http.createServer(function (request, response) {
   response.writeHead(404);
   response.end();
 });
-server.listen(8080, function () {
-  console.log("Listening on port : 8080");
-});
-
-webSocketServer = new WebSocketServer({
-  httpServer: server,
-  autoAcceptConnections: false,
-});
-
-function iSOriginAllowed(origin) {
-  return true;
-}
-
-webSocketServer.on("request", function (request) {
-  if (!iSOriginAllowed(request.origin)) {
-    request.reject();
-    console.log(" Connection from : " + request.origin + " rejected.");
-    return;
-  }
-
-  var connection = request.accept("echo-protocol", request.origin);
-  console.log(" Connection accepted : " + request.origin);
-  connection.on("message", function (message) {
-    if (message.type === "utf8") {
-      console.log("Received Message: " + message.utf8Data);
-    }
-  });
-  //   consumer.on("message", function (message) {
-  //     console.log(message);
-  //     connection.sendUTF(message.value);
-  //   });
-  connection.on("close", function (reasonCode, description) {
-    console.log("Connection " + connection.remoteAddress + " disconnected.");
-  });
+server.listen(9099, function () {
+  console.log("Listening on port : 9099");
 });
